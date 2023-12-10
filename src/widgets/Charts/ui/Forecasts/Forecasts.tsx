843f3d84c5febd6c7b104d0a5317938519f9d1c3
seraf1m <serafimkremnev1008@gmail.com>
2023-12-10 22:17:16 +0300

    fix

--- src/widgets/Charts/ui/Forecasts/Forecasts.tsx
+++ src/widgets/Charts/ui/Forecasts/Forecasts.tsx
@@ -10,7 +10,6 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { currencyNextDays } from "@/shared/data/currency";
 
 ChartJS.register(
   CategoryScale,
@@ -37,33 +36,5 @@ export default function Forecasts({}: Props) {
     },
   };
 
-  const data = {
-    labels: currencyNextDays.dates,
-    datasets: [
-      {
-        label: "Евро",
-        data: currencyNextDays.CNY,
-        borderColor: "rgb(92, 92, 199)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-      {
-        label: "Юань",
-        data: currencyNextDays.CNY,
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-      {
-        label: "Доллар",
-        data: currencyNextDays.USD,
-        borderColor: "rgb(246, 161, 99)",
-        backgroundColor: "rgba(246, 161, 99, 0.5)",
-      },
-    ],
-  };
-
-  return (
-    <div>
-      <Line data={data} options={options} />
-    </div>
-  );
+  return <div></div>;
 }
